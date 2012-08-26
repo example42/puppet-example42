5ac88247a079bc327bf1cee34af1a07ab1e0e745
Alessandro Franceschi <al@lab42.it>
2012-08-26 16:29:30 +0200

    Added sample vagrant role

---
+++ manifests/role/role_vagrant.pp
@@ -0,0 +1,19 @@
+class example42::role::role_mysql {
+
+  class { 'vagrant': }
+
+  vagrant::environment { 'example42':
+    user => 'example42',
+  }
+  vagrant::vm { 'test_lenny64':
+    environment => 'example42',
+    box         => 'debian_lenny_64',
+    box_url     => 'http://puppetlabs.s3.amazonaws.com/pub/debian_lenny_64.box',
+  }
+  vagrant::vm { "test_lucid64":
+      environment => "example4242" ,
+      box => "lucid64" ,
+      box_url => "http://files.vagrantup.com/lucid64.box"
+  }
+
+}

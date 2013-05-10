2ba8ed58e4c0fd9c5917dd3079f0116be60c6891
Alessandro Franceschi <al@lab42.it>
2013-05-10 15:58:20 +0200

    Added sample minimal class

---
+++ manifests/minimal.pp
@@ -0,0 +1,23 @@
+# Class example42::minimal
+#
+# A Class that installs the minimal prerequisites to
+# seamless usage of Example42 modules
+#
+class example42::minimal {
+
+# Basic package management
+case $::osfamily {
+  redhat: {
+    include yum::repo::epel
+    package { 'redhat-lsb': ensure => present }
+  }
+  debian: {
+    include apt
+    package { 'lsb-release': ensure => present }
+  }
+  suse: {
+    package { 'lsb': ensure => present }
+  }
+}
+
+}
